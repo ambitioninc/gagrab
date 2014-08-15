d885357c53b7bd22cd575242ff3cb3b027c64a9d
Erik Swanson <theerikswanson@gmail.com>
2014-08-14 22:38:18 -0400

    Nice doc strings.

--- gagrab/grabber.py
+++ gagrab/grabber.py
@@ -4,49 +4,72 @@ from six.moves import zip
 
 
 class Grabber(object):
-    def __init__(self, authorized_service):
-        """A ``Grabber`` object stores authetication information, and provides
-        a convenient interface to query data from google analytics.
+    """A ``Grabber`` object stores authetication information, and provides
+    a convenient interface to query data from google analytics.
 
-        :type authroized_service: service_account_auth.AuthorizedService
-        :param authorized_service: An AuthorizedService object,
-             authorized for the 'analytics' service, version 'v3'.
-        """
+    An authorized_service must be set up first, according to the steps
+    shown in the `gclient-service-account-auth
+    <https://github.com/ambitioninc/gclient-service-account-auth>`
+    docs.
+    """
+    def __init__(self, authorized_service):
         self.project_id = authorized_service.project_id
         self.service = authorized_service.service
 
     def query(self, view, dimensions, metrics, start_date, end_date, **kwargs):
         """Run a query on the account's Google Analytics data.
 
-        Required Arguments:
+        Queries run with this method are subject to the sampling
+        imposed by the core-reporting api.
 
-            view - the integer id representing the view to be
+        :param view: The integer id representing the view to be
             queried. This can be found in the Google Analytics admin
-            section "View Settings"
+            section "View Settings" and will look similar to
+            "UA-00000-1"
 
-            dimensions - a list of dimensions to aggregate
+        :param dimensions: A list of dimensions to aggregate
             on. E.g. ['operatingSystem', 'dimension1']
 
-            metrics - a list of the names ometrics to be included in
+        :param metrics: - a list of the names ometrics to be included in
             the query. E.g. ['pageviews', 'timeOnPage']
 
-            start_date - a datetime for the start of the query.
+        :param start_date: - a date for the start of the query.
 
-            end_date - a datetime for the end of the query.
+        :param end_date: - a date for the end of the query.
 
         **kwargs:
 
-            - Any additional query parameter can be passed in to the
-            key word argument of this this function. These will not be
-            automatically converted from python datatypes into
-            appropriate google syntax. These should only be passed if
-            you know what yo're doing.
+        Any additional query parameter can be passed in to the keyword
+        arguments of this this function, and will be passed along to
+        the underlying google analytics query. These will not be
+        automatically converted from python datatypes into appropriate
+        google syntax. These should only be passed if you know what
+        yo're doing.
 
-        Returns:
-
-            - data a list of dictionaries mapping the requested
+        :returns: A list of dictionaries mapping each requested
             dimension/metric to its value for each row.
 
+        A query like
+
+        .. code-block:: python
+
+            my_grabber.query(
+                view='UA-00000-1',
+                dimensions=['browser'],
+                metrics=['pageviews'],
+                start_date='2014-07-01',
+                end_date='2014-07-01'
+            )
+
+        Would return data like
+
+        .. code-block:: python
+
+            [{'browser': 'firefox', 'pageviews': 1264},
+             {'browser': 'chrome', 'pageviews': 2782},
+             {'browser': 'safari', 'pageviews': 805},
+             {'browser': 'internet explorer', 'pageviews': 3397},
+            ]
         """
         query_response = self._query_response(
             view, dimensions, metrics, start_date, end_date, **kwargs
@@ -74,7 +97,17 @@ class Grabber(object):
 
 
 def data_from_query_response(query_response):
-    """Format GA queries to a list of dicts.
+    """Format a GA query response to a list of dicts.
+
+    :type query_response: dict
+
+    :param query_response: The raw response returned by the google
+    analytics api.
+
+    The structure returned by the google analytics api contains a lot
+    of superfluous data, most of which is not in native python
+    formats. This function converts the data to an appropriate python
+    format and retuns a nice list of dictionaries.
     """
     convert = {
         'STRING': lambda x: x,
